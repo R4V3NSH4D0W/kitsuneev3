4ca882500664388b13e6f03c7e6f04ded259037e
R4V3NSH4D0W <lenishmagar@gmail.com>
2024-12-28 19:30:25 +0545

    MK-03: check if internet is Rechable or not

--- src/helper/network-helper.tsx
+++ src/helper/network-helper.tsx
@@ -2,14 +2,16 @@ import NetInfo from '@react-native-community/netinfo';
 
 export const checkInternetConnection = async (): Promise<boolean> => {
   const state = await NetInfo.fetch();
-  return state.isConnected ?? false;
+  return (state.isConnected && state.isInternetReachable) ?? false;
 };
 
 export const subscribeToInternetConnection = (
   callback: (isConnected: boolean) => void,
 ): (() => void) => {
   const unsubscribe = NetInfo.addEventListener(state => {
-    callback(state.isConnected ?? false);
+    const isConnected =
+      (state.isConnected && state.isInternetReachable) ?? false;
+    callback(isConnected);
   });
   return unsubscribe;
 };
