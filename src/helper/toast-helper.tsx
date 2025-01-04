231d231049a630504dde94ffe15965892a814a78
R4V3NSH4D0W <lenishmagar@gmail.com>
2025-01-04 20:43:27 +0545

    MK-06: set Toast duration to 1sec

--- src/helper/toast-helper.tsx
+++ src/helper/toast-helper.tsx
@@ -4,10 +4,12 @@ export const showToast = (
   type: 'success' | 'error' | 'info',
   text1: string,
   text2?: string,
+  duration: number = 1000,
 ) => {
   Toast.show({
     type: type,
     text1: text1,
     text2: text2,
+    visibilityTime: duration,
   });
 };
